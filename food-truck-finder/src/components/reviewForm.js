7f7486b95b6bee5672ca43d0fef33b2233267b26
fenin <kongkenan@gmail.com>
2022-08-05 10:43:31 -0700

    leave a review form

--- food-truck-finder/src/components/reviewForm.js
+++ food-truck-finder/src/components/reviewForm.js
@@ -1,16 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import './form.css'
 
-function reviewForm () {
+const ReviewForm = () => {
+    
+    const { id } = useParams()
+    const [myForm, setMyForm] = useState(' ')
+
+    useEffect(() => {
+		//Write your get/fetch here
+		axios.get(`https://young-anchorage-22001.herokuapp.com/foodtrucks/${id}`)
+            .then((res) => {
+                setMyForm(res.data.reviews)
+            })
+	}, [id]);
 
     return (
         // <div className="review-form" onSubmit={handleSubmit}>
-            <form className="sign-up">
-                <label className="form">Sign Up</label>
-                <input type="text"></input>
-                <button type="submit">Review</button>
+            <form
+            className="sign-up">
+            <div>
+                <label 
+                className="form">
+                Leave a Review
+                </label>
+            </div>
+                <input 
+                className="form-box" 
+                type="text"
+                placeholder="Write your review">
+                </input>
+            <div>
+                <button 
+                className="submit-button" 
+                type="submit">
+                    Submit Review
+                </button>
+            </div>
             </form>
         // </div>
     )
 }
 
-export default reviewForm
+export default ReviewForm
