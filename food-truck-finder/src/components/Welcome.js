208f0119bcd8029831b11e54d73e1cc8435a719f
luuxe <luxelily@gmail.com>
2022-08-10 17:14:45 -0500

    styled welcome page

--- food-truck-finder/src/components/Welcome.js
+++ food-truck-finder/src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
     return (
         <div className='flex flex-col items-center bg-[#7ed957] h-screen'>
             <img src={logo} alt='grub truck logo' className='w-64 md:w-72 absolute'/>
-            <div className='flex flex-col justify-center items-center w-3/4 md:w-1/4 h-3/4 mt-20 shadow-lg rounded-lg bg-white'>
+            <div className='flex flex-col justify-center items-center w-3/4 md:w-1/2 h-3/4 mt-20 shadow-lg rounded-lg bg-white'>
             <Link to='/register' className='shadow-lg flex-shrink-0 py-2 px-8 mb-5 border rounded-lg'>Get Started</Link>
             <Link to='/login' className='bg-[#7ed957] shadow-lg flex-shrink-0 py-2 px-8 mb-5 border rounded-lg'>Login</Link>
             <Link to='/foodtrucks' className='text-sm mt-5'>continue as a guest</Link>
